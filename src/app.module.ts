62a20a9b6867ab7273acae33f5e87ef7358f8dab
web77code <web77@yandex.ru>
2023-03-19 08:39:31 +0300

    feat: add entities and enable auto synchronize to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,11 @@ import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
 
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { Offer } from './offers/entities/offer.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -18,8 +23,8 @@ import { OffersModule } from './offers/offers.module';
       username: 'student',
       password: 'student',
       database: 'kupipodariday',
-      entities: [],
-      synchronize: false,
+      entities: [User, Wish, Wishlist, Offer],
+      synchronize: true,
     }),
     UsersModule,
     WishesModule,
