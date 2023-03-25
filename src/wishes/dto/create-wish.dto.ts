bb3b35b3a94f41637b5f3cfd1bd0fa7ac34252d9
web77code <web77@yandex.ru>
2023-03-25 17:44:00 +0300

    fix: corrected default values for several columns

--- src/wishes/dto/create-wish.dto.ts
+++ src/wishes/dto/create-wish.dto.ts
@@ -1,1 +1,29 @@
-export class CreateWishDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreateWishDto {
+  @IsString()
+  @Length(1, 250)
+  name: string;
+
+  @IsOptional()
+  @IsUrl()
+  link: string;
+
+  @IsOptional()
+  @IsUrl()
+  image: string;
+
+  @IsNotEmpty()
+  price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 1024)
+  description: string;
+}
