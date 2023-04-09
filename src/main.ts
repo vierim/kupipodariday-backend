2af3b59cc54029dc7bb7c48b0c341d8959e14e7e
vierim <web77@yandex.ru>
2023-04-09 12:31:54 +0300

    fix: delete interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
-import { RemovePasswordInterceptor } from './interceptors/remove-password.interceptor';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalInterceptors(new RemovePasswordInterceptor());
 
   await app.listen(3001);
 }
